c8e38a82f23884fcfe1f56fbc3e691f81e7b6a9c
Olga Letkova <olja.let52@gmail.com>
2020-08-30 17:02:31 +0300

    Changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice;
-        // TicketPrise подставляем с копейками, два последних знака-копейки.
-        int OneMile;
-        // OneMile подставляем с копейками.
-        TicketPrice = 550000;
-        OneMile = 2000;
-        int QuantityOfMiles = TicketPrice / OneMile;
-        System.out.println(QuantityOfMiles);
+        int ticketPrice = 550000;
+        int oneMile = 2000;
+        // Значения переменных ticketPrise и oneMile подставляем с копейками, два последних знака-копейки.
+        int quantityOfMiles = ticketPrice / oneMile;
+        System.out.println(quantityOfMiles);
     }
 }
